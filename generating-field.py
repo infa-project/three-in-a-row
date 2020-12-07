9fb7a2c102a462dad013d2c499238c7fb8e2e1fa
Ivan Russkikh <55546067+iv-russ@users.noreply.github.com>
2020-12-07 12:15:02 +0300

    "generating field" function
    Added function that generates field without three in a row

---
+++ generating-field.py
@@ -0,0 +1,22 @@
+from random import choice
+
+
+LIST_OF_COLORS = ['r', 'g', 'b', 'y', 'p']
+N = 10
+
+
+def is_there_three_in_a_row(arr):
+    for i in range(N):
+        for j in range(N - 2):
+            if arr[i][j] == arr[i][j + 1] == arr[i][j + 2] or \
+                    arr[j][i] == arr[j + 1][i] == arr[j + 2][i]:
+                return True
+    return False
+
+
+def generate_field_list():
+    field_list = [[choice(LIST_OF_COLORS) for _ in range(N)] for _ in range(N)]
+    while is_there_three_in_a_row(field_list):
+        field_list = [[choice(LIST_OF_COLORS) for _ in range(N)] for _ in range(N)]
+    return field_list
+
